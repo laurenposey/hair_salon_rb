7cf1d4956855f5941f9d814d271d150d97bf7a9e
Lauren Posey <mrslaurenposey@gmail.com>
2016-07-15 10:59:42 -0700

    pass spec: all method, returns empty array at first

--- lib/client.rb
+++ lib/client.rb
@@ -1,1 +1,21 @@
+class Client
+  attr_reader(:name, :id)
 
+  define_method(:initialize) do |attributes|
+    @name = attributes.fetch(:name)
+    @id = attributes[:id]
+  end
+
+  define_singleton_method(:all) do
+    returned_clients = DB.exec("SELECT * FROM clients;")
+    clients = []
+    returned_clients.each() do |client|
+      name = client.fetch("name")
+      id = doc.fetch("id")
+      clients.push(Client.new({:name => name, :id => id}))
+    end
+    clients
+  end
+
+
+end
--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -10,5 +10,6 @@ DB = PG.connect({:dbname => "hair_salon_test"})
 RSpec.configure do |config|
   config.after(:each) do
     DB.exec("DELETE FROM stylists *;")
+    DB.exec("DELETE FROM clients *;")
   end
 end
