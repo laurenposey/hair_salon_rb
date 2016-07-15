1335f24803302a8a5e699b956e1ed1549384ac27
Lauren Posey <mrslaurenposey@gmail.com>
2016-07-15 11:03:16 -0700

    pass spec: will display the client name

--- spec/client_spec.rb
+++ spec/client_spec.rb
@@ -8,4 +8,11 @@ describe(Client) do
     end
   end
 
+  describe('#name') do
+    it('will display the client name') do
+      test_client = Client.new({:name => 'Emma', :id => nil})
+      expect(test_client.name()).to(eq('Emma'))
+    end
+  end
+
 end
