5753e109a2f61d424a112f17272d5e50cae5b276
Lauren Posey <mrslaurenposey@gmail.com>
2016-07-20 21:33:06 -0700

    CRUD implementation completed still need to do more integration specs

--- app.rb
+++ app.rb
@@ -17,39 +17,42 @@ get('/stylists') do
   erb(:stylists)
 end
 
-get("/stylists/new") do
+get("/stylists/new")do
   erb(:stylist_form)
 end
 
-post("/stylists") do #User submits form Server grabs the attributes submitted through the form and uses them to create a new object. The server returns the stylists page with the new stylist added
+post('/stylists') do
   name = params.fetch("name")
-  @stylist = Stylist.new({:name => name, :id => nil})
-  @stylist.save()
+  stylist = Stylist.new({:name => name, :id => nil})
+  stylist.save()
+  @stylists = Stylist.all()
   erb(:stylists)
 end
+
 
 get("/stylists/:id") do
   @stylist = Stylist.find(params.fetch("id").to_i())
   erb(:stylist)
 end
 
-get('/stylists/:id/edit') do
+get("/stylists/:id/edit") do
+  @stylist = Stylist.find(params.fetch("id").to_i())
   erb(:stylist_edit_form)
 end
 
-patch('stylists/:id') do
-  name = params.fetch('name')
-  @stylist = Stylist.find(params.fetch('id').to_i())
+patch("/stylists/:id") do
+  name = params.fetch("name")
+  @stylist = Stylist.find(params.fetch("id").to_i())
   @stylist.update({:name => name})
-  erb(:stylist)
 end
 
-get('/clients') do
+get("/clients") do
   @clients = Client.all()
   erb(:clients)
 end
 
-get('clients/new') do
+get('/clients/new') do
+  @stylists = Stylist.all()
   erb(:client_form)
 end
 
@@ -59,7 +62,8 @@ post("/clients") do
   @stylist = Stylist.find(stylist_id)
   @client = Client.new({:name => name, :id => nil, :stylist_id => stylist_id})
   @client.save()
-  erb(:stylist)
+  @clients = Client.all()
+  erb(:clients)
 end
 
 get("/clients/:id") do
@@ -68,27 +72,29 @@ get("/clients/:id") do
 end
 
 get("/clients/:id/edit") do
+  @client = Client.find(params.fetch("id").to_i())
   erb(:client_edit_form)
 end
 
 patch("/clients/:id") do
   name = params.fetch("name")
   stylist_id = params.fetch("stylist_id").to_i()
+  @stylist = Stylist.find(stylist_id)
   @client = Client.find(params.fetch("id").to_i())
   @client.update({:name => name, :stylist_id => stylist_id})
   erb(:clients)
 end
 
-delete("/stylists/:id/edit") do
+delete("/stylists/:id/delete") do
   @stylist = Stylist.find(params.fetch("id").to_i())
   @stylist.delete()
   @stylists = Stylist.all()
   erb(:stylists)
 end
 
-delete("/clients/:id/edit") do
+delete("/clients/:id/delete") do
   @client = Client.find(params.fetch("id").to_i())
   @client.delete()
-  @client = Client.all()
+  @clients = Client.all()
   erb(:clients)
 end
